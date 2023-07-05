896517a77d8a22e1e0fd6d5de90ba2b32c501cfa
Saisanthosh645 <122589587+Saisanthosh645@users.noreply.github.com>
2023-07-05 18:17:31 +0530

    Web development

---
+++ Quote Generator/script.js
@@ -0,0 +1,31 @@
+async function fetchQuote(){
+    try{
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        return data.content + "-" + data.author;
+    } catch(error) {
+        console.log("Error : " + error);
+        return "Failed to fetch a quote. Please try again later."
+    }
+}
+
+
+
+async function generateQuote(){
+    const quoteElement = document.querySelector("#quote");
+    quoteElement.textContent = "Fetching Quote....";
+    const quote = await fetchQuote();
+    quoteElement.textContent = quote;
+}
+
+const copyBtn = document.querySelector("#copyBtn");
+copyBtn.addEventListener("click", () => {
+    const quoteElement = document.querySelector("#quote");
+    const quote = quoteElement.textContent;
+    navigator.clipboard.writeText(quote).then(()=>{alert("Quote is copied!")})
+})
+
+const generateBtn = document.querySelector("#generateBtn");
+generateBtn.addEventListener("click", ()=>{
+    generateQuote();
+})
